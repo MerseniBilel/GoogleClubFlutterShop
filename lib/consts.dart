1ec04c366b69efa0d287ebcdd881d8b80bf5b0a5
MerseniBilel <bilelmerseni7016@gmail.com>
2020-12-01 20:41:29 +0100

    welcome to flutter

---
+++ lib/consts.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const kTextColor = Color(0xFF535353);
+const kTextlightcolor = Color(0xFFACACAC);
+
+const defaultPadding = 20.0;
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:GoogleClub/consts.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
